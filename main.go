0b9f0a6dc6bd845685178c53a794354ea325e5d0
dio404 <75159857+dio404@users.noreply.github.com>
2022-08-10 18:46:05 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	code := []byte{1,5,1,7,10,0}
+	ret, _ := Run(code)
+	fmt.Println(ret)
+}
---
+++ opcodes.go
@@ -0,0 +1,7 @@
+package main
+
+const(
+	PUSH1=1
+	ADD=10
+	RETURN=0
+)
