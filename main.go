6c64d928ac21be1afba7c80d7eecb620a1bde890
wqluo <1722341878@qq.com>
2022-08-11 11:08:15 +0800

    add gitignore & go.mod

---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/importer"
+	"go/parser"
+	"go/token"
+	"go/types"
+	"os"
+
+	"golang.org/x/tools/go/ssa"
+	"golang.org/x/tools/go/ssa/ssautil"
+)
+
+// const hello = `
+// package main
+// import "fmt"
+// var a int = 1
+// var b int = 2
+
+// func main() {
+// 	if a+b >0 {fmt.Println("hello")}
+// }
+// `
+
+const hello2 = `
+package main
+
+import "fmt"
+	
+func add(a int, b int) int {
+	return a + b
+}
+
+func main() {
+	a := 1
+	b := 2
+	c := a + b
+	d := add(c, 3)
+	fmt.Println(d)
+}
+`
+
+const hello = `
+package main
+
+var a int
+var b int
+
+func main() {
+	a = 4
+	b = 7
+	c := a + b
+	_ = c
+}
+`
+
+func main() {
+	// Replace interface{} with any for this test.
+	// ssa.SetNormalizeAnyForTesting(true)
+	// defer ssa.SetNormalizeAnyForTesting(false)
+	// Parse the source files.
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, "hello.go", hello, parser.ParseComments)
+	if err != nil {
+		fmt.Print(err) // parse error
+		return
+	}
+	files := []*ast.File{f}
+
+	// Create the type-checker's package.
+	pkg := types.NewPackage("hello", "")
+
+	// Type-check the package, load dependencies.
+	// Create and build the SSA program.
+	hello, _, err := ssautil.BuildPackage(
+		&types.Config{Importer: importer.Default()}, fset, pkg, files, ssa.SanityCheckFunctions)
+	if err != nil {
+		fmt.Print(err) // type error in some package
+		return
+	}
+
+	// Print out the package.
+	hello.WriteTo(os.Stdout)
+
+	// Print out the package-level functions.
+	hello.Func("init").WriteTo(os.Stdout)
+	hello.Func("main").WriteTo(os.Stdout)
+	// hello.Func("add").WriteTo(os.Stdout)
+}
